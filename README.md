89f17e687ad378c0cd3271477292a5c9daadc387
iam-dev <elky.bachtiar@duo.nl>
2022-04-30 14:17:10 +0200

    completed assignment1

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ Successfully generated 5 typings!
 Compiled 2 Solidity files successfully
 Greeter deployed to: 0xc275De5912f076B3310AaE05B148eA877918d11f
 ```
+
+## Assignment 1
+Program a super simple “Hello World” smart contract: write a storeNumber function to store an unsigned integer and then a retrieveNumber function to retrieve it. Clearly comment your code. Once completed, deploy the smart contract on remix. Push the .sol file to Github or Gist and include a screenshot of the Remix UI once deployed in your final submission pdf.
--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -5,6 +5,8 @@ import "hardhat/console.sol";
 
 contract Greeter {
     string private greeting;
+    // assignment 1) write a storeNumber function to store an unsigned integer
+    uint256 private _storeNumber;
 
     constructor(string memory _greeting) {
         console.log("Deploying a Greeter with greeting:", _greeting);
@@ -15,8 +17,19 @@ contract Greeter {
         return greeting;
     }
 
+    //assignment 1) Program a super simple “Hello World” smart contract: write a storeNumber function to store an unsigned integer and then a retrieveNumber function to retrieve it.
+    function retrieveNumber() public view returns (uint256) {
+        return _storeNumber;
+    }
+
     function setGreeting(string memory _greeting) public {
         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
         greeting = _greeting;
+    }
+
+    function setStoreNumber(uint256 storeNumber_) public {
+        require(storeNumber_ > 0, "storeNumber_ is the zero value");
+        require(storeNumber_ != _storeNumber, "_storeNumber already set");
+        _storeNumber = storeNumber_;
     }
 }
